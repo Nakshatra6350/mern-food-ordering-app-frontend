dd439de8ccf3f7ae1dc91dd35bb07e1ce538e6e3
Nakshatra6350 <nakshatragarg678@gmail.com>
2024-02-22 17:36:12 +0530

    second frontend commit

---
+++ src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,25 @@
+import {
+  useCreateMyRestaurant,
+  useGetMyRestaurant,
+  useUpdateMyRestaurant,
+} from "@/api/MyRestaurantApi";
+import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
+
+const ManageRestaurantPage = () => {
+  const { createRestaurant, isLoading: isCreatedLoading } =
+    useCreateMyRestaurant();
+  const { restaurant } = useGetMyRestaurant();
+  const { updateRestaurant, isLoading: isUpdatedLoading } =
+    useUpdateMyRestaurant();
+
+  const isEditing = !!restaurant;
+  return (
+    <ManageRestaurantForm
+      restaurant={restaurant}
+      onSave={isEditing ? updateRestaurant : createRestaurant}
+      isLoading={isCreatedLoading || isUpdatedLoading}
+    />
+  );
+};
+
+export default ManageRestaurantPage;
